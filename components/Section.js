4cfb77dae0d67649be5e449dbe00183c84a44836
Lethabarb <colininja@outlook.com.au>
2025-08-02 11:08:02 +1000

    Adjusted section to span rows instead of colums when in mobile. Added section height as a prop with default 35rem.

--- components/Section.js
+++ components/Section.js
@@ -1,11 +1,11 @@
-export default function Section({children, textColor, bgColor}) {
+export default function Section({children, textColor, bgColor, height = '35rem'}) {
     let text_col = TextColour(textColor)
     let bg_col = BgColour(bgColor)
     let title_col = TitleColour(bgColor)
     return (
         <div className="flex flex-col justify-center items-center w-full py-4">
             <h2 className={title_col + " text-5xl mb-5"} >ABOUT ME</h2>
-            <div className={"grid grid-cols-[1fr_2fr] h-[35rem] w-full justify-start py-4 "+bg_col+" " + text_col + " p-20"}>
+            <div className={"grid lg:grid-cols-[1fr_2fr] grid-cols-1 grid-rows-[1fr 2fr] h-["+height+"] w-full justify-start py-4 "+bg_col+" " + text_col + " p-5 lg:p-20"}>
                 {children}
             </div>
         </div>
