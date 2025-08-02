ef02fa4f31e894d8d1d5708a0e59e0824b4a0c01
Lethabarb <colininja@outlook.com.au>
2025-08-02 10:59:17 +1000

    Made header mobile friendly by changing grid size to 2 and including a burger menu when screen is small. Installed: HeadlessUi and HeroIcons

--- components/Header.js
+++ components/Header.js
@@ -1,48 +1,99 @@
 import Link from "next/link";
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 export default function Header() {
     return (
-        <div className="
-            grid grid-cols-3
+        <Disclosure as="nav">
+            <div className="
+            grid grid-cols-2
             text-2xl 
             bg-slate-100 shadow-2xl text-blue-700  border-b-blue-700 border-b-2
-            p-0 m-0">
-            <div className="
-            flex justify-around items-center flex-row
+            p-0 m-0
+            lg:grid-cols-3">
+                <div className="
+            lg:flex justify-around items-center flex-row hidden
             ">
-                <div className="p-2 
+                    <div className="p-2 
                 bg-gradient-to-b from-slate-50 from-50% to-blue-900 to-50% gradient-size
                 hover:text-slate-50 
                 h-full w-full
                 flex justify-center items-center
                 "><Link href="/">Home</Link></div>
-                <div className="p-2 
+                    <div className="p-2 
                 bg-gradient-to-b from-slate-50 from-50% to-blue-900 to-50% gradient-size
                 hover:text-slate-50 
                 h-full w-full
                 flex justify-center items-center
             "><Link href="/Projects">Projects</Link></div>
 
-            </div>
+                </div>
+                <div className="inset-y-0 left-0 flex items-center lg:hidden ml-2 p-2">
+                    {/* Mobile menu button*/}
+                    <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-blue-800 focus:ring-2 focus:ring-slate-700 focus:outline-hidden focus:ring-inset">
+                        <span className="absolute -inset-0.5" />
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                        <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+                    </DisclosureButton>
+                </div>
                 <div className="
-                flex justify-center items-center
-                text-6xl text-blue-50 w-full 
-                bg-blue-800 h-full pl-10 pr-10 p-2"><Link href="/">Oliver Robin</Link></div>
-            <div className="flex justify-around items-center flex-row">
-                <div className="p-2 
+                flex justify-center items-center col-start-2
+                text-3xl lg:text-6xl text-blue-50 w-full text-center
+                bg-blue-800 h-full pl-10 pr-10 p-2
+                "><Link href="/">Oliver Robin</Link></div>
+                <div className="lg:flex justify-around items-center flex-row hidden">
+                    <div className="p-2 
                 bg-gradient-to-b from-slate-50 from-50% to-blue-900 to-50% gradient-size
                 hover:text-slate-50 
                 h-full w-full
                 flex justify-center items-center
                 "><Link href="/Experience">Experience</Link></div>
-                <div className="p-2 
+                    <div className="p-2 
                 bg-gradient-to-b from-slate-50 from-50% to-blue-900 to-50% gradient-size
                 hover:text-slate-50 
                 h-full w-full
                 flex justify-center items-center
                 "><Link href="/Contact">Contact</Link></div>
 
+                </div>
             </div>
-        </div>
+            <DisclosurePanel transition className="lg:hidden
+                origin-top transition duration-200 ease-out data-closed:-translate-y-6 data-closed:opacity-0
+                w-[50vw]">
+                <div className="
+                flex justify-center items-center col-start-2 flex-col
+                text-3xl lg:text-6xl text-blue-800 w-full text-center
+                bg-slate-50 h-full pl-10 pr-10 p-2
+                shadow
+                ">
+                    <div className="p-2 
+                h-full w-full
+                flex justify-center items-center
+                ">
+                        <Link href="/Experience">Home</Link>
+                    </div>
+                    <div className="p-2 
+                h-full w-full
+                flex justify-center items-center
+                ">
+                        <Link href="/Contact">Projects</Link>
+                    </div>
+                    <div className="p-2 
+                h-full w-full
+                flex justify-center items-center
+                ">
+                        <Link href="/Contact">Experience</Link>
+                    </div>
+                    <div className="p-2 
+                h-full w-full
+                flex justify-center items-center
+                ">
+                        <Link href="/Contact">Contact</Link>
+                    </div>
+
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
     )
 }
